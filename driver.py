e06a47236f1a26c1ae8e6341337a77652052d23e
leone <791960492@qq.com>
2024-03-25 22:02:45 +0800

    update

--- driver.py
+++ driver.py
@@ -9,14 +9,15 @@ import openai
 import logging
 import sys
 
-PYTHON_FILE = "~/anaconda3/envs/sf/bin/python"
+BASE_PYTHON_FILE = "~/anaconda3//bin/python"
+SF_PYTHON_FILE = "~/anaconda3/envs/sf/bin/python"
 DEFAULT_OPENAI_MODEL = "gpt-3.5-turbo"
 
 USE_PYTHON_CODEBLOCK_INSTRUCTION = "Write your response using Python code blocks. For example:\n```python\nprint('Hello world!')\n```"
 GEN_SPU_CODE_INSTRUCTION = f"""
 You are an artificial intelligence that responds only with python code. SecretFlow provides a Python API for the Security Processing Unit (SPU) to enable privacy-preserving computation. You are required to complete an SPU code template. What needs to be completed is the '# <Some descriptions>' in the `func`, `get_alice_data`, `get_bob_data` and `import` sections. You will be given sample SPU code and you should generate more complicated SPU code.
 
-JAX is a Python library for high-performance numerical computation, and jax.numpy is as compatible with numpy functionality as possible. For example `jnp.array([10])` is the jax version of `np.array([10])`, `jnp.add(data_alice, data_bob)` is the jax version of `np.add(data_alice, data_bob)` . Now for SPU code you should use jax.numpy instead of numpy. Some libraries (such as sklearn) are not compatible with jax, do not use them.
+JAX is a Python library for high-performance numerical computation, and jax.numpy is as compatible with numpy functionality as possible. For example `jnp.array([10])` is the jax version of `np.array([10])`, `jnp.add(data_alice, data_bob)` is the jax version of `np.add(data_alice, data_bob)` . You should use jax.numpy instead of numpy. Some libraries (such as sklearn) are not compatible with jax, do not use them.
 
 For "func", "get_alice_data" and "get_bob_data", do not use any random functions, do not add Python decorators, and these functions must be independent of SecretFlow and SPU.
 """
@@ -111,22 +112,22 @@ print("== RESULT BEGIN ==\n%s\n%s\n== RESULT END ==" % (revealed_result, reveale
 sf.shutdown()
 """
 SAMPLE_NORMAL_CODE = """
-import numpy as np
+import jax.numpy as jnp
 
 def func(data_alice, data_bob):
     # Compute the sum of data from alice and bob
-    ret = np.add(data_alice, data_bob)
-    return ret, np.size(ret)
+    ret = jnp.add(data_alice, data_bob)
+    return ret, jnp.size(ret)
 
 # Function to get alice's data
 def get_alice_data():
     # Here alice's data is [10]
-    return np.array([10])
+    return jnp.array([10])
 
 # Function to get bob's data
 def get_bob_data():
     # Here bob's data is [5]
-    return np.array([5])
+    return jnp.array([5])
 
 # Get data
 data_alice = get_alice_data()
@@ -147,7 +148,7 @@ You have been asked to generate Python code before. However, you gave an unexpec
 GEN_NORMAL_CODE_INSTRUCTION = """
 You are an artificial intelligence that responds only with python code. SecretFlow provides a Python API for the Security Processing Unit (SPU) to enable privacy-preserving computation. You will be given the complete SPU code and need to generate normal python code (you will be given a template of the normal code) which is required to perform the same calculations as the SPU code, but in normal mode (non-SPU mode).
 
-JAX is a Python library for high-performance numerical computation, and jax.numpy is as compatible with numpy functionality as possible. For example `jnp.array([10])` is the jax version of `np.array([10])`, `jnp.add(data_alice, data_bob)` is the jax version of `np.add(data_alice, data_bob)` . Now for normal code you should use numpy instead of jax.numpy.
+JAX is a Python library for high-performance numerical computation, and jax.numpy is as compatible with numpy functionality as possible. For example `jnp.array([10])` is the jax version of `np.array([10])`, `jnp.add(data_alice, data_bob)` is the jax version of `np.add(data_alice, data_bob)` . You should use jax.numpy instead of numpy. Some libraries (such as sklearn) are not compatible with jax, do not use them.
 
 For "func", "get_alice_data" and "get_bob_data", do not use any random functions, do not add Python decorators, and these functions must be independent of SecretFlow and SPU.
 """
@@ -254,26 +255,6 @@ class ChatProxy:
             response = completion.choices[0].message.content
         return response
 
-    def update(self, requirement, orig_response, **kwargs):
-        print(">>> Update")
-        # print(
-        #     f">>> Update\n{requirement}\n[Previous request]\n{orig_request}\n\n[Previous reponse]\n{orig_response}"
-        # )
-        return self.send(
-            temperature=0.5,
-            messages=[
-                {
-                    "role": "system",
-                    "content": f"{requirement}",
-                },
-                {
-                    "role": "user",
-                    "content": f"[Previous reponse]\n{orig_response}",
-                },
-            ],
-            **kwargs,
-        )
-
 
 def py_is_syntax_valid(code: str) -> bool:
     try:
@@ -283,11 +264,11 @@ def py_is_syntax_valid(code: str) -> bool:
         return False
 
 
-def get_py_code(proxy, orig_response, **kwargs):
+def get_py_code(proxy, orig_inst, orig_prompt, orig_response, **kwargs):
     response = orig_response
     while True:
         py_code = re.search(
-            "```python(.*?)```",
+            r"```.*?\n(.*?)```",
             response,
             re.DOTALL,
         )
@@ -295,27 +276,73 @@ def get_py_code(proxy, orig_response, **kwargs):
         if py_code:
             return py_code.group(1)
 
-        response = proxy.update(
-            RELECT_USE_PYTHON_CODEBLOCK_INSTRUCTION, response, **kwargs
+        print(">>> Update Python Code")
+        inst = f"{orig_inst}\n{RELECT_USE_PYTHON_CODEBLOCK_INSTRUCTION}"
+        prompt = f"{orig_prompt}\n[Bad Previous Response]\n{response}"
+        response = proxy.send(
+            messages=[
+                {
+                    "role": "system",
+                    "content": inst,
+                },
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+            **kwargs,
         )
 
 
-def get_valid_py_code(proxy, orig_response, **kwargs):
+def get_valid_py_code(proxy, orig_inst, orig_prompt, orig_response, **kwargs):
     response = orig_response
     while True:
-        code = get_py_code(proxy, response, **kwargs)
+        code = get_py_code(proxy, orig_inst, orig_prompt, response, **kwargs)
         if py_is_syntax_valid(code):
             return code
 
-        response = proxy.update(
-            RELECT_GEN_VALID_PYTHON_CODE_INSTRUCTION, response, **kwargs
+        print(">>> Update Syntax Valid Python Code")
+        inst = f"{orig_inst}\n{RELECT_GEN_VALID_PYTHON_CODE_INSTRUCTION}"
+        prompt = f"{orig_prompt}\n[Bad Previous Response]\n{response}"
+        response = proxy.send(
+            messages=[
+                {
+                    "role": "system",
+                    "content": inst,
+                },
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+            **kwargs,
         )
+
+
+def get_seeds(path: str):
+    seeds = {}
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            with open(os.path.join(root, file)) as f:
+                f_lines = f.readlines()
+                for i, line in enumerate(f_lines):
+                    if "[SPU Code Path]" in line:
+                        _spu_code_path = f_lines[i + 1].replace("\n", "")
+                        with open(_spu_code_path) as _spu_code_f:
+                            _spu_code = _spu_code_f.read()
+                    if "[Communication Bytes]" in line:
+                        _comm_bytes = int(f_lines[i + 1].replace("\n", ""))
+            seeds[_spu_code] = _comm_bytes
+    return seeds
 
 
 def main():
     proxy = ChatProxy()
     round = 0
     previous_spu_code_queue = {SAMPLE_SPU_CODE: 0}
+    previous_spu_code_queue = {**previous_spu_code_queue, **get_seeds("./same")}
+    previous_spu_code_queue = {**previous_spu_code_queue, **get_seeds("./different")}
+
     error_cases = []
     while True:
         previous_spu_code = max(
@@ -323,28 +350,30 @@ def main():
         )
 
         round += 1  # Start from 1
+
         print(f"== Round {round} ==")
         print(f">>> Request to generate SPU code")
-        request = f"{GEN_SPU_CODE_PROMPT}\n[Sample SPU code]\n```python\n{previous_spu_code}\n```\n"
+        inst = f"{GEN_SPU_CODE_INSTRUCTION}\n{USE_PYTHON_CODEBLOCK_INSTRUCTION}\n"
+        prompt = f"{GEN_SPU_CODE_PROMPT}\n[Sample SPU code]\n```python\n{previous_spu_code}\n```\n"
         if len(error_cases) > 0:
-            request += "Here are some error cases and their outputs, avoid to make same mistakes\n"
+            prompt += "Here are some error cases and their outputs, avoid to make same mistakes\n"
             for case, output in error_cases:
-                request += f"[Error Case]\n{case}\n[Output]\n{output}\n"
+                prompt += f"[Error Case]\n{case}\n[Output]\n{output}\n"
         response = proxy.send(
             messages=[
                 {
                     "role": "system",
-                    "content": f"{GEN_SPU_CODE_INSTRUCTION}\n{USE_PYTHON_CODEBLOCK_INSTRUCTION}\n",
+                    "content": inst,
                 },
                 {
                     "role": "user",
-                    "content": request,
+                    "content": prompt,
                 },
             ],
         )
         print(f"<<< Get SPU code")
 
-        spu_code = get_valid_py_code(proxy, response)
+        spu_code = get_valid_py_code(proxy, inst, prompt, response)
         spu_code_with_comm = re.sub(
             r"(.*)(sf.shutdown())",
             r"\1\ncomm_byte_nums = spu_node_device.get_comm_byte_nums()\nprint('[Number of SPU Communication Bytes] %d' % (sum(comm_byte_nums) / len(comm_byte_nums)))\n\2",
@@ -353,68 +382,89 @@ def main():
         )
 
         spu_code_hash = hashlib.sha256(spu_code.encode()).hexdigest()
-        spu_code_file_name = str(spu_code_hash) + "_spu_code.py"
+        spu_code_path = os.path.abspath(
+            os.path.join("seeds", str(spu_code_hash) + "_spu_code.py")
+        )
         os.makedirs("seeds", exist_ok=True)
-        with open(os.path.join("seeds", spu_code_file_name), "w") as f:
+        with open(spu_code_path, "w") as f:
             f.write(spu_code_with_comm)
 
         print(f">>> Request to generate normal code")
+        inst = f"{GEN_NORMAL_CODE_INSTRUCTION}\n{USE_PYTHON_CODEBLOCK_INSTRUCTION}\n"
+        prompt = f"{GEN_NORMAL_CODE_PROMPT}\n```python\n{spu_code}\n```"
         response = proxy.send(
             messages=[
                 {
                     "role": "system",
-                    "content": f"{GEN_NORMAL_CODE_INSTRUCTION}\n{USE_PYTHON_CODEBLOCK_INSTRUCTION}\n",
+                    "content": inst,
                 },
                 {
                     "role": "user",
-                    "content": f"{GEN_NORMAL_CODE_PROMPT}\n```python\n{spu_code}\n```",
+                    "content": prompt,
                 },
             ],
         )
         print(f"<<< Get normal code")
 
-        normal_code = get_valid_py_code(proxy, response)
+        normal_code = get_valid_py_code(proxy, inst, prompt, response)
         # normal_code_hash = hashlib.sha256(normal_code.encode()).hexdigest()
-        normal_code_file_name = str(spu_code_hash) + "_normal_code.py"
-        with open(os.path.join("seeds", normal_code_file_name), "w") as f:
+        normal_code_path = os.path.abspath(
+            os.path.join("seeds", str(spu_code_hash) + "_normal_code.py")
+        )
+        with open(normal_code_path, "w") as f:
             f.write(normal_code)
 
-        spu_result = subprocess.run(
-            [
-                os.path.expanduser(PYTHON_FILE),
-                os.path.join("seeds", spu_code_file_name),
-            ],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            env={"JAX_PLATFORMS": "cpu"},
+        rand_in_spu_code = re.search(r"^[^#]*?random", spu_code.lower(), re.MULTILINE)
+        rand_in_normal_code = re.search(
+            r"^[^#]*?random", normal_code.lower(), re.MULTILINE
         )
-        normal_result = subprocess.run(
-            [
-                os.path.expanduser(PYTHON_FILE),
-                os.path.join("seeds", normal_code_file_name),
-            ],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-        )
+        if rand_in_spu_code or rand_in_normal_code:
+            spu_output = "error: should not use random"
+            normal_output = "error: should not use random"
+        else:
+            spu_output = subprocess.run(
+                [
+                    os.path.expanduser(SF_PYTHON_FILE),
+                    spu_code_path,
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                env={"JAX_PLATFORMS": "cpu"},
+            )
+            normal_output = subprocess.run(
+                [
+                    os.path.expanduser(BASE_PYTHON_FILE),
+                    normal_code_path,
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
 
-        spu_result_with_comm = spu_result.stdout.decode()
+            spu_output = spu_output.stdout.decode()
+            normal_output = normal_output.stdout.decode()
 
         result_match = re.search(
-            "== RESULT BEGIN ==\n(.*)== RESULT END ==", spu_result_with_comm, re.DOTALL
+            r"== RESULT BEGIN ==\n(.*)== RESULT END ==", spu_output, re.DOTALL
         )
         spu_result = result_match.group(1) if result_match else "NO_RESULT"
-        if not result_match:
+        if spu_result == "NO_RESULT":
             while len(error_cases) >= 1:
                 error_cases.pop(0)
-            error_lines = [
-                line
-                for line in spu_result_with_comm.split("\n")
-                if "error" in line.lower() or "fail" in line.lower()
-            ]
-            error_cases.append((spu_code, "\n".join(error_lines)))
+            error_cases.append(
+                (
+                    spu_code,
+                    "\n".join(
+                        [
+                            line
+                            for line in spu_output.split("\n")
+                            if "error" in line.lower() or "fail" in line.lower()
+                        ]
+                    ),
+                )
+            )
 
         spu_comm_match = re.search(
-            r"\[Number of SPU Communication Bytes\](.*)", spu_result_with_comm
+            r"\[Number of SPU Communication Bytes\](.*)", spu_output
         )
         spu_comm = int(spu_comm_match.group(1).strip()) if spu_comm_match else -1
         while len(previous_spu_code_queue) >= MAX_SEED_QUEUE_SIZE:
@@ -422,41 +472,65 @@ def main():
             del previous_spu_code_queue[min_key]
         previous_spu_code_queue[spu_code] = spu_comm
 
-        normal_result = normal_result.stdout.decode()
         result_match = re.search(
-            "== RESULT BEGIN ==\n(.*)== RESULT END ==", normal_result, re.DOTALL
+            r"== RESULT BEGIN ==\n(.*)== RESULT END ==", normal_output, re.DOTALL
         )
         normal_result = result_match.group(1) if result_match else "NO_RESULT"
 
-        print(f">>> Request to compare output")
         all_output = f"[Execution Result of SPU Code]\n{spu_result}\n[Execution Result of Normal Code]\n{normal_result}\n"
-        response = proxy.send(
-            messages=[
-                {"role": "system", "content": COMPARE_RESULT_INSTRUCTION},
-                {
-                    "role": "user",
-                    "content": f"{COMPARE_RESULT_PROMPT}\n{all_output}\n[Comparison Result]",
-                },
-            ],
-        )
-        print(f"<<< Get response")
+        if spu_result == "NO_RESULT" and normal_result == "NO_RESULT":
+            response = "FAIL\nSPU code and normal code have problems\n"
+        elif spu_result == "NO_RESULT":
+            response = "FAIL\nSPU code has problems\n"
+        elif normal_result == "NO_RESULT":
+            response = "FAIL\nNormal code has problems\n"
+        else:
+            print(f">>> Request to compare output")
+            response = proxy.send(
+                messages=[
+                    {"role": "system", "content": COMPARE_RESULT_INSTRUCTION},
+                    {
+                        "role": "user",
+                        "content": f"{COMPARE_RESULT_PROMPT}\n{all_output}\n",
+                    },
+                ],
+            )
+            print(f"<<< Get response")
+            response = re.sub(
+                r"^.*?Comparison Result.*?\n", "", response, flags=re.MULTILINE
+            )
+            response = re.sub(r"^\s*\n", "", response, flags=re.MULTILINE)
 
-        response = re.sub(r"^\s*\n", "", response, flags=re.MULTILINE)
         result = response.split("\n", 1)[0].lower()
 
         if result not in ("same", "fail", "different"):
             result = "unknown"
 
-        result_file_name = str(spu_code_hash) + "_result.py"
+        result_file_path = os.path.abspath(
+            os.path.join(result, str(spu_code_hash) + "_result.py")
+        )
         os.makedirs(result, exist_ok=True)
-        with open(os.path.join(result, result_file_name), "w") as f:
-            f.write(f"{all_output}\n{response}\n"
-                    f"{os.path.abspath(os.path.join("seeds", spu_code_file_name))}\n"
-                    f"{os.path.abspath(os.path.join("seeds", normal_code_file_name))}\n"
-                    f"Communication Bytes: {spu_comm}")
-        print(os.path.join(result, result_file_name))
-        print(os.path.join("seeds", spu_code_file_name))
-        print(os.path.join("seeds", normal_code_file_name))
+        with open(result_file_path, "w") as f:
+            f.write(
+                f"{all_output}\n\n"
+                + f"[Comparison Result]\n{response}\n\n"
+                + f"[SPU Code Path]\n{spu_code_path}\n\n"
+                + f"[Normal Code Path]\n{normal_code_path}\n\n"
+                + f"[Communication Bytes]\n{spu_comm}\n\n"
+                + (
+                    f"[SPU Error Msg]\n{spu_output}\n"
+                    if spu_result == "NO_RESULT"
+                    else ""
+                )
+                + (
+                    f"[Normal Error Msg]\n{normal_output}\n"
+                    if normal_result == "NO_RESULT"
+                    else ""
+                )
+            )
+        print(result_file_path)
+        print(spu_code_path)
+        print(normal_code_path)
 
 
 if __name__ == "__main__":
